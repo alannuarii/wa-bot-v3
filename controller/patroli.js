53192b1d8b3a94176b5beb38fc7f9a490e185e8c
alannuarii <alannuariii@gmail.com>
2024-01-03 16:02:18 +0800

    first commit

---
+++ controller/patroli.js
@@ -0,0 +1,20 @@
+const db = require('../db/connection');
+
+const { promisify } = require("util");
+const dbQuery = promisify(db.query).bind(db);
+
+const checkData = async (client, groupId) => {
+    try {
+        const sql = "SELECT patroli.nama, patroli.waktu FROM note_patroli JOIN patroli ON note_patroli.kode = patroli.kode ORDER BY note_patroli.id_npatroli DESC LIMIT ?";
+        const values = [1];
+        const result = await dbQuery(sql, values);
+        if (result) {
+            await client.sendMessage(groupId, 'Automatic message from Bot!');
+        }
+    } catch (error) {
+        console.error("Kesalahan koneksi ke database:", error);
+
+    }
+}
+
+module.exports = { checkData };
